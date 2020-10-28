4bfe18dec5b8f00b4fd632857adc76c61d39d7d3
Svetlana Antipova <die-chimaere@yandex.ru>
2020-10-28 17:06:56 +0300

    Updates

---
+++ precache-manifest.4dc40b7d7c89d63c65a1778261e22d68.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b619579dcb93e8cc4ed8880c6aa2532",
+    "url": "/news-explorer-frontend/index.html"
+  },
+  {
+    "revision": "4b83f7d041692a22db23",
+    "url": "/news-explorer-frontend/static/css/main.f2fe405d.chunk.css"
+  },
+  {
+    "revision": "c1e95973e5de190cfb81",
+    "url": "/news-explorer-frontend/static/js/2.c4b16f25.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/news-explorer-frontend/static/js/2.c4b16f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b83f7d041692a22db23",
+    "url": "/news-explorer-frontend/static/js/main.10cce116.chunk.js"
+  },
+  {
+    "revision": "30563db5395ebfdbb83c",
+    "url": "/news-explorer-frontend/static/js/runtime-main.0fc9bfae.js"
+  },
+  {
+    "revision": "d94383b574906bf6c3cca5cc20f63d55",
+    "url": "/news-explorer-frontend/static/media/close-icon.d94383b5.svg"
+  },
+  {
+    "revision": "4b12ed391db0482184458e5c7b2a1224",
+    "url": "/news-explorer-frontend/static/media/fb.4b12ed39.svg"
+  },
+  {
+    "revision": "f734cc7fa408fa3829aad9e3d5aca099",
+    "url": "/news-explorer-frontend/static/media/github.f734cc7f.svg"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/news-explorer-frontend/static/media/inter-regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "dd4abe07ad7e8e8293daa6ea63f41992",
+    "url": "/news-explorer-frontend/static/media/main-backgound.dd4abe07.png"
+  },
+  {
+    "revision": "175a96ce472d1ca73e7bfaaabddbb721",
+    "url": "/news-explorer-frontend/static/media/not-found.175a96ce.svg"
+  },
+  {
+    "revision": "39d7226d1f440dd7e02dd0c0424e6b43",
+    "url": "/news-explorer-frontend/static/media/roboto-black.39d7226d.woff"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/news-explorer-frontend/static/media/roboto-medium.6422eac7.woff"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/news-explorer-frontend/static/media/roboto-regular.58bf8e0b.woff"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "/news-explorer-frontend/static/media/robotoslab-regular.594bcf8b.woff"
+  },
+  {
+    "revision": "49d8d8285e8bc3047dac602de489bfb0",
+    "url": "/news-explorer-frontend/static/media/sourcesanspro-regular.49d8d828.woff"
+  }
+]);
