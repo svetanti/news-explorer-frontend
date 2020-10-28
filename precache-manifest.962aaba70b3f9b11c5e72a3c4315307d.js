5038c3e62399fa1a8e61e7146da951561a22e6ef
Svetlana Antipova <die-chimaere@yandex.ru>
2020-10-28 17:00:34 +0300

    Updates

---
+++ precache-manifest.962aaba70b3f9b11c5e72a3c4315307d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f5a30e283b44a8e003533c73de9828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aff1f82c3a947b33b13",
+    "url": "/static/css/main.879c7deb.chunk.css"
+  },
+  {
+    "revision": "c1e95973e5de190cfb81",
+    "url": "/static/js/2.c4b16f25.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c4b16f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aff1f82c3a947b33b13",
+    "url": "/static/js/main.dacb7edc.chunk.js"
+  },
+  {
+    "revision": "c60840167acfbb45b912",
+    "url": "/static/js/runtime-main.18fc895e.js"
+  },
+  {
+    "revision": "d94383b574906bf6c3cca5cc20f63d55",
+    "url": "/static/media/close-icon.d94383b5.svg"
+  },
+  {
+    "revision": "4b12ed391db0482184458e5c7b2a1224",
+    "url": "/static/media/fb.4b12ed39.svg"
+  },
+  {
+    "revision": "f734cc7fa408fa3829aad9e3d5aca099",
+    "url": "/static/media/github.f734cc7f.svg"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/static/media/inter-regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "dd4abe07ad7e8e8293daa6ea63f41992",
+    "url": "/static/media/main-backgound.dd4abe07.png"
+  },
+  {
+    "revision": "175a96ce472d1ca73e7bfaaabddbb721",
+    "url": "/static/media/not-found.175a96ce.svg"
+  },
+  {
+    "revision": "39d7226d1f440dd7e02dd0c0424e6b43",
+    "url": "/static/media/roboto-black.39d7226d.woff"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/static/media/roboto-medium.6422eac7.woff"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/static/media/roboto-regular.58bf8e0b.woff"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "/static/media/robotoslab-regular.594bcf8b.woff"
+  },
+  {
+    "revision": "49d8d8285e8bc3047dac602de489bfb0",
+    "url": "/static/media/sourcesanspro-regular.49d8d828.woff"
+  }
+]);
