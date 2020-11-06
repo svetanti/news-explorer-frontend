ce45f702bd93acaf6776067acc1b67c079072016
Svetlana Antipova <die-chimaere@yandex.ru>
2020-11-06 19:38:06 +0300

    add utils

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import { NewsContext } from '../../contexts/NewsContext';
+import { articlesDeclension, numberDeclension, adjectiveDeclination } from '../../utils/utils';
 
 export default function SavedNewssaved(props) {
   const { isLoggedIn } = props;
@@ -19,40 +20,19 @@ export default function SavedNewssaved(props) {
     .sort((a, b) => b.quantity - a.quantity)
     .map(item => item.keyword);
 
-  const articlesDeclension = ['статья', 'статьи', 'статей'][
-    savedNews.length === 0 && 0
-      && savedNews.length % 10 === 1 && savedNews.length % 100 !== 11
-      ? 0
-      : savedNews.length % 10 >= 2
-        && savedNews.length % 10 <= 4 && (savedNews.length % 100 < 10 || savedNews.length % 100 >= 20)
-        ? 1
-        : 2];
-
-  const numberDeclension = ['му', 'м', 'и'][
-    keywordsSorted.slice(3).length % 10 === 1
-      && keywordsSorted.slice(3).length % 100 !== 11
-      ? 0
-      : keywordsSorted.slice(3).length % 10 >= 2
-        && keywordsSorted.slice(3).length % 10 <= 4
-        && (keywordsSorted.slice(3).length % 100 < 10 || keywordsSorted.slice(3).length % 100 >= 20)
-        ? 1
-        : 2];
-
-  const adjectiveDeclination = ['другому', 'другим'][
-    keywordsSorted.slice(3).length % 10 === 1
-      && keywordsSorted.slice(3).length % 100 !== 11
-      ? 0
-      : 1];
-
   const keywordsToRender = keywordsSorted.length <= 3
     ? keywordsSorted.join(', ')
-    : `${keywordsSorted.slice(0, 3).join(', ')} и ${keywordsSorted.slice(3).length}-${numberDeclension} ${adjectiveDeclination}`;
+    : `${keywordsSorted
+      .slice(0, 3)
+      .join(', ')} и ${keywordsSorted
+        .slice(3)
+        .length}-${numberDeclension(keywordsSorted)} ${adjectiveDeclination(keywordsSorted)}`;
 
   return (
     <div className='saved-news-header'>
       <p className='saved-news-header__title'>Сохранённые статьи</p>
       <h2 className='saved-news-header__heading'>
-        {currentUser.name}, у вас {savedNews.length} сохранённых {articlesDeclension}</h2>
+        {currentUser.name}, у вас {savedNews.length} сохранённых {articlesDeclension(savedNews)}</h2>
       <p className='saved-news-header__keywords-list'>По ключевым словам:&nbsp;
       <span className='saved-news-header__keywords'>{keywordsToRender}</span></p>
     </div>
